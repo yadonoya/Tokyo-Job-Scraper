a855e617acc90dfdf84decc27f1d1fc4273714c3
yadonoya <mscottschreiber@gmail.com>
2022-05-25 20:33:34 +0900

    feat: add a temple url that a user can modify based on their search needs

--- job-scraper.py
+++ job-scraper.py
@@ -3,3 +3,12 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
+def get_url(pos,loc):
+    template_url = 'https://jp.indeed.com/jobs?q={}&l={}'
+    pos = pos.replace(' ', '+')
+    loc = loc.replace(' ', '+')
+    url = template_url.format(pos, loc)
+    return url
+
+url = get_url('Full-stack Developer', '東京都')
+print(url)
